85e1fa4c8fdfd751f030e7430f8a477dc644e3fd
Rich Wareham <rich@richwareham.com>
2016-08-12 11:22:31 +0100

    add preliminary support for TL16C550 serial

--- src/mame/drivers/buri.cpp
+++ src/mame/drivers/buri.cpp
@@ -8,9 +8,23 @@
 
 #include "bus/rs232/rs232.h"
 #include "cpu/g65816/g65816.h"
+#include "machine/ins8250.h"
 #include "machine/mos6551.h"
 #include "sound/3812intf.h"
 #include "video/tms9928a.h"
+
+const char* MOS6551_TAG = "mos6551";
+const char* YM3812_TAG = "ym3812";
+const char* TMS9918_TAG = "tms9918";
+const char* NS16550_TAG = "ns16550";
+
+const char* UART1_TAG = "uart1";
+const char* UART2_TAG = "uart2";
+
+const int MOS6551_START = 0xDFFC;
+const int TMS9918_START = 0xDE00;
+const int YM3812_START = 0xDE02;
+const int NS16550_START = 0xDE10;
 
 class buri_state : public driver_device
 {
@@ -18,20 +32,18 @@ public:
 	buri_state(const machine_config &mconfig, device_type type, const char *tag) :
 	           driver_device(mconfig, type, tag),
 	           m_maincpu(*this, "maincpu"),
-	           m_acia(*this, "acia")
+	           m_mos6551(*this, MOS6551_TAG),
+		   m_ns16550(*this, NS16550_TAG)
 	{ }
-
-	DECLARE_DRIVER_INIT(buri);
-	DECLARE_MACHINE_START(buri);
-	DECLARE_MACHINE_RESET(buri);
 
 	DECLARE_WRITE_LINE_MEMBER(acia_irq_w);
 
 	required_device<cpu_device> m_maincpu;
-	optional_device<mos6551_device> m_acia;
+	optional_device<mos6551_device> m_mos6551;
+	optional_device<ns16550_device> m_ns16550;
 
 	// interrupts
-	int m_acia_irq;
+	int m_mos6551_irq;
 };
 
 static ADDRESS_MAP_START(buri_mem, AS_PROGRAM, 8, buri_state)
@@ -41,14 +53,25 @@ static ADDRESS_MAP_START(buri_mem, AS_PROGRAM, 8, buri_state)
 	AM_RANGE(0xE0C000, 0xE0FFFF) AM_RAM
 
 	// VDP
-	AM_RANGE(0xDE00, 0xDE00) AM_DEVREADWRITE("tms9918", tms9918a_device, vram_read, vram_write)
-	AM_RANGE(0xDE01, 0xDE01) AM_DEVREADWRITE("tms9918", tms9918a_device, register_read, register_write)
+	AM_RANGE(TMS9918_START, TMS9918_START)
+		AM_DEVREADWRITE(TMS9918_TAG, tms9918a_device,
+				vram_read, vram_write)
+	AM_RANGE(TMS9918_START + 1, TMS9918_START + 1)
+		AM_DEVREADWRITE(TMS9918_TAG, tms9918a_device,
+				register_read, register_write)
 
 	// ACIA
-	AM_RANGE(0xDFFC, 0xDFFF) AM_DEVREADWRITE("acia", mos6551_device, read, write)
+	AM_RANGE(MOS6551_START, MOS6551_START + 3)
+		AM_DEVREADWRITE(MOS6551_TAG, mos6551_device, read, write)
+
+	// UART2
+	AM_RANGE(NS16550_START, NS16550_START + 7)
+		AM_DEVREADWRITE(NS16550_TAG, ns16550_device,
+				ins8250_r, ins8250_w)
 
 	// SOUND
-	AM_RANGE(0xDE02, 0xDE03) AM_DEVREADWRITE("ym3812", ym3812_device, read, write)
+	AM_RANGE(YM3812_START, YM3812_START + 1)
+		AM_DEVREADWRITE(YM3812_TAG, ym3812_device, read, write)
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START(buri)
@@ -67,27 +90,48 @@ static MACHINE_CONFIG_START(buri, buri_state)
 	MCFG_CPU_ADD("maincpu", G65816, XTAL_2MHz)
 	MCFG_CPU_PROGRAM_MAP(buri_mem)
 
-	MCFG_DEVICE_ADD("acia", MOS6551, 0)
+	MCFG_DEVICE_ADD(MOS6551_TAG, MOS6551, 0)
 	MCFG_MOS6551_XTAL(XTAL_1_8432MHz)
-	MCFG_MOS6551_TXD_HANDLER(DEVWRITELINE("uart", rs232_port_device, write_txd))
+	MCFG_MOS6551_TXD_HANDLER(DEVWRITELINE(UART1_TAG, rs232_port_device, write_txd))
 	MCFG_MOS6551_IRQ_HANDLER(WRITELINE(buri_state, acia_irq_w))
 
-	MCFG_RS232_PORT_ADD("uart", default_rs232_devices, "pty")
-	MCFG_RS232_RXD_HANDLER(DEVWRITELINE("acia", mos6551_device, write_rxd))
-	MCFG_RS232_DCD_HANDLER(DEVWRITELINE("acia", mos6551_device, write_dcd))
-	MCFG_RS232_DSR_HANDLER(DEVWRITELINE("acia", mos6551_device, write_dsr))
-	MCFG_RS232_CTS_HANDLER(DEVWRITELINE("acia", mos6551_device, write_cts))
+	MCFG_RS232_PORT_ADD(UART1_TAG, default_rs232_devices, "pty")
+	MCFG_RS232_RXD_HANDLER(
+		DEVWRITELINE(MOS6551_TAG, mos6551_device, write_rxd))
+	MCFG_RS232_DCD_HANDLER(
+		DEVWRITELINE(MOS6551_TAG, mos6551_device, write_dcd))
+	MCFG_RS232_DSR_HANDLER(
+		DEVWRITELINE(MOS6551_TAG, mos6551_device, write_dsr))
+	MCFG_RS232_CTS_HANDLER(
+		DEVWRITELINE(MOS6551_TAG, mos6551_device, write_cts))
+
 	MCFG_DEVICE_CARD_DEVICE_INPUT_DEFAULTS("pty", terminal)
 	MCFG_DEVICE_CARD_DEVICE_INPUT_DEFAULTS("terminal", terminal)
 
-	MCFG_DEVICE_ADD("tms9918", TMS9918A, XTAL_10_738635MHz)
+	MCFG_DEVICE_ADD(NS16550_TAG, NS16550, 0)
+	/*
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE(
+		UART2_TAG, rs232_port_device, write_txd))
+
+	MCFG_RS232_PORT_ADD(UART2_TAG, default_rs232_devices, "pty")
+	MCFG_RS232_RXD_HANDLER(
+		DEVWRITELINE(NS16550_TAG, ns16550_device, rx_w))
+	MCFG_RS232_DCD_HANDLER(
+		DEVWRITELINE(NS16550_TAG, ns16550_device, dcd_w))
+	MCFG_RS232_DSR_HANDLER(
+		DEVWRITELINE(NS16550_TAG, ns16550_device, dsr_w))
+	MCFG_RS232_CTS_HANDLER(
+		DEVWRITELINE(NS16550_TAG, ns16550_device, cts_w))
+	*/
+
+	MCFG_DEVICE_ADD(TMS9918_TAG, TMS9918A, XTAL_10_738635MHz)
 	MCFG_TMS9928A_VRAM_SIZE(0x4000)
 	// MCFG_TMS9928A_OUT_INT_LINE_CB(INPUTLINE("maincpu", INPUT_LINE_IRQ0))
 	MCFG_TMS9928A_SCREEN_ADD_NTSC( "screen" )
-	MCFG_SCREEN_UPDATE_DEVICE( "tms9918", tms9918a_device, screen_update )
+	MCFG_SCREEN_UPDATE_DEVICE( TMS9918_TAG, tms9918a_device, screen_update )
 
 	MCFG_SPEAKER_STANDARD_MONO("mono")
-	MCFG_SOUND_ADD("ym3812", YM3812, XTAL_3_579545MHz)
+	MCFG_SOUND_ADD(YM3812_TAG, YM3812, XTAL_3_579545MHz)
 	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 1.00)
 MACHINE_CONFIG_END
 
@@ -95,19 +139,6 @@ ROM_START(buri)
 	ROM_REGION(0x2000, "maincpu", 0)
 	ROM_LOAD("burios.bin", 0x0000, 0x2000, CRC(e527d758))
 ROM_END
-
-DRIVER_INIT_MEMBER(buri_state, buri)
-{
-}
-
-MACHINE_START_MEMBER(buri_state, buri)
-{
-}
-
-MACHINE_RESET_MEMBER(buri_state, buri)
-{
-	m_acia->reset();
-}
 
 WRITE_LINE_MEMBER(buri_state::acia_irq_w)
 {
