12a11b615010de2265daf7f18332fa34cfad3946
Rich Wareham <rjw57@cam.ac.uk>
2016-11-14 14:20:35 +0000

    use TMS9929A instead of TMS9918A in VDP

--- src/mame/drivers/buri.cpp
+++ src/mame/drivers/buri.cpp
@@ -15,12 +15,12 @@
 
 const char* MOS6551_TAG = "mos6551";
 const char* YM3812_TAG = "ym3812";
-const char* TMS9918_TAG = "tms9918";
+const char* TMS9929_TAG = "tms9929";
 
 const char* UART1_TAG = "uart1";
 
 const int MOS6551_START = 0xDFFC;
-const int TMS9918_START = 0xDE00;
+const int TMS9929_START = 0xDE00;
 const int YM3812_START = 0xDE02;
 
 class buri_state : public driver_device
@@ -29,22 +29,24 @@ public:
 	buri_state(const machine_config &mconfig, device_type type, const char *tag) :
 	           driver_device(mconfig, type, tag),
 	           m_maincpu(*this, "maincpu"),
-	           m_mos6551(*this, MOS6551_TAG)
+	           m_mos6551(*this, MOS6551_TAG),
+	           m_tms2998a(*this, TMS9929_TAG)
 	{
 		m_irqs.val = 0;
 	}
 
 	DECLARE_WRITE_LINE_MEMBER(mos6551_irq_w);
-	DECLARE_WRITE_LINE_MEMBER(tms9918a_irq_w);
+	DECLARE_WRITE_LINE_MEMBER(tms9929a_irq_w);
 
 	required_device<cpu_device> m_maincpu;
 	optional_device<mos6551_device> m_mos6551;
+	optional_device<tms9929a_device> m_tms2998a;
 
 	union {
 		uint32_t val;
 		struct {
 			int mos6551 : 1;
-			int tms9918a : 1;
+			int tms9929a : 1;
 		} flags;
 	} m_irqs;
 
@@ -64,11 +66,11 @@ static ADDRESS_MAP_START(buri_mem, AS_PROGRAM, 8, buri_state)
 	AM_RANGE(0xE0C000, 0xE0FFFF) AM_RAM
 
 	// VDP
-	AM_RANGE(TMS9918_START, TMS9918_START)
-		AM_DEVREADWRITE(TMS9918_TAG, tms9918a_device,
+	AM_RANGE(TMS9929_START, TMS9929_START)
+		AM_DEVREADWRITE(TMS9929_TAG, tms9929a_device,
 				vram_read, vram_write)
-	AM_RANGE(TMS9918_START + 1, TMS9918_START + 1)
-		AM_DEVREADWRITE(TMS9918_TAG, tms9918a_device,
+	AM_RANGE(TMS9929_START + 1, TMS9929_START + 1)
+		AM_DEVREADWRITE(TMS9929_TAG, tms9929a_device,
 				register_read, register_write)
 
 	// ACIA
@@ -114,11 +116,11 @@ static MACHINE_CONFIG_START(buri, buri_state)
 	MCFG_DEVICE_CARD_DEVICE_INPUT_DEFAULTS("pty", terminal)
 	MCFG_DEVICE_CARD_DEVICE_INPUT_DEFAULTS("terminal", terminal)
 
-	MCFG_DEVICE_ADD(TMS9918_TAG, TMS9918A, XTAL_10_738635MHz)
+	MCFG_DEVICE_ADD(TMS9929_TAG, TMS9929A, XTAL_10_738635MHz)
 	MCFG_TMS9928A_VRAM_SIZE(0x4000)
-	MCFG_TMS9928A_OUT_INT_LINE_CB(WRITELINE(buri_state, tms9918a_irq_w))
-	MCFG_TMS9928A_SCREEN_ADD_NTSC( "screen" )
-	MCFG_SCREEN_UPDATE_DEVICE( TMS9918_TAG, tms9918a_device, screen_update )
+	MCFG_TMS9928A_OUT_INT_LINE_CB(WRITELINE(buri_state, tms9929a_irq_w))
+	MCFG_TMS9928A_SCREEN_ADD_PAL( "screen" )
+	MCFG_SCREEN_UPDATE_DEVICE( TMS9929_TAG, tms9929a_device, screen_update )
 
 	MCFG_SPEAKER_STANDARD_MONO("mono")
 	MCFG_SOUND_ADD(YM3812_TAG, YM3812, XTAL_3_579545MHz)
@@ -137,10 +139,10 @@ WRITE_LINE_MEMBER(buri_state::mos6551_irq_w)
 	irqs_updated_();
 }
 
-WRITE_LINE_MEMBER(buri_state::tms9918a_irq_w)
+WRITE_LINE_MEMBER(buri_state::tms9929a_irq_w)
 {
-	// TMS9918A IRQ line is active *low*
-	m_irqs.flags.tms9918a = (state == 0);
+	// TMS9929A IRQ line is active *low*
+	m_irqs.flags.tms9929a = (state == 0);
 	irqs_updated_();
 }
 
