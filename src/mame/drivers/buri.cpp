b00b0b2106fbfd58d94ce21f13812a12deba7905
Rich Wareham <rjw57@cam.ac.uk>
2016-11-14 13:59:59 +0000

    remove 16550 UART for the moment

--- src/mame/drivers/buri.cpp
+++ src/mame/drivers/buri.cpp
@@ -29,7 +29,7 @@ public:
 	buri_state(const machine_config &mconfig, device_type type, const char *tag) :
 	           driver_device(mconfig, type, tag),
 	           m_maincpu(*this, "maincpu"),
-	           m_mos6551(*this, MOS6551_TAG),
+	           m_mos6551(*this, MOS6551_TAG)
 	{
 		m_irqs.val = 0;
 	}
